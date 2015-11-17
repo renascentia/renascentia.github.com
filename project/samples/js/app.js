136c41d7341fd0851e57872e8d79369a5a1c1242
Rodrigo Parra <rodrigokparra@gmail.com>
2015-11-17 04:05:19 -0200

    Slider fixes

--- project/samples/js/app.js
+++ project/samples/js/app.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('.top-bar .search-icon').click(function(event) {
+      if ($('.top-bar-section li.search-field').hasClass('open')){
+        $('.top-bar-section li.search-field').animate({width: "0px", margin: "0px",display: 'none'}, 500).removeClass('open')
+      } else {
+        $('.top-bar-section li.search-field').animate({width: "160px", margin: "0 20px 0 0",display: 'block'}, 500).addClass('open')
+      }
+  });
